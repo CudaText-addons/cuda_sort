681d56ec1ab933d1b66f26c502e43a9580ba4b8e
Alexey-T <support@uvviewsoft.com>
2016-06-10 10:43:21 +0300

    Numeric

--- __init__.py
+++ __init__.py
@@ -12,12 +12,21 @@ def get_offsets():
         return r[0], r[2]
     else:
         return -1, -1
+
+
+def get_num_and_text(s):
+    n = 0
+    while n<len(s) and s[n].isdigit(): n += 1
+    num = int(s[:n])
+    text = s[n:]
+    return (num, text)
         
 
 def do_sort(is_reverse, 
             is_nocase, 
             del_dups=False, 
             del_blanks=True,
+            is_numeric=False,
             offset1=-1, 
             offset2=-1):
 
@@ -52,9 +61,16 @@ def do_sort(is_reverse,
         lines = list(set(lines))
 
     is_new_api = hasattr(cudatext, 'CONVERT_LINE_TABS_TO_SPACES')
+
     def _key(item):
         s = item
-        if is_nocase: s = s.lower()
+        if is_nocase: 
+            s = s.lower()
+
+        if is_numeric:        
+            num, text = get_num_and_text(s)
+            #print('parts "%s": %d %s' % (s, num, text))
+            s = '%20.20d ' % num + text
 
         if (offset1>=0) or (offset2>=0):
             if is_new_api:
@@ -63,8 +79,12 @@ def do_sort(is_reverse,
             if offset1>=0: s = s[offset1:]
             
         return s
-    
-    lines = sorted(lines, key=_key, reverse=is_reverse)
+
+        
+    lines = sorted(lines, 
+        key=_key, 
+        reverse=is_reverse
+        )
 
     if is_all:
         ed.set_text_all('\n'.join(lines)+'\n')
@@ -77,25 +97,28 @@ def do_sort(is_reverse,
         + (', all text' if is_all else '') \
         + (', reverse' if is_reverse else '') \
         + (', ignore case' if is_nocase else '') \
+        + (', numeric' if is_numeric else '') \
         + (', offsets %d..%d' % (offset1, offset2) if (offset1>=0) or (offset2>=0) else '')
     msg_status(text)
 
 
 def do_dialog():
     size_x = 330
-    size_y = 220
+    size_y = 240
     id_rev = 0
     id_nocase = 1
     id_del_dup = 2
     id_del_sp = 3
-    id_offset1 = 6
-    id_offset2 = 8
-    id_ok = 9
+    id_numeric = 4
+    id_offset1 = 7
+    id_offset2 = 9
+    id_ok = 10
     
     op_rev = ini_read(fn_ini, section, 'rev', '0')
     op_nocase = ini_read(fn_ini, section, 'nocase', '0')
     op_del_dup = ini_read(fn_ini, section, 'del_dup', '1')
     op_del_sp = ini_read(fn_ini, section, 'del_sp', '1')
+    op_numeric = ini_read(fn_ini, section, 'numeric', '0')
     
     op_offset1, op_offset2 = get_offsets()
     
@@ -105,16 +128,17 @@ def do_dialog():
       c1.join(['type=check', 'pos=6,30,300,0', 'cap=&Ignore case', 'val='+op_nocase]),
       c1.join(['type=check', 'pos=6,54,300,0', 'cap=Delete d&uplicate lines', 'val='+op_del_dup]),
       c1.join(['type=check', 'pos=6,78,300,0', 'cap=Delete &blank lines', 'val='+op_del_sp]),
-      c1.join(['type=label', 'pos=6,106,300,0', 'cap=Sort only by substring, offsets 0-based:']),
-      c1.join(['type=label', 'pos=30,128,130,0', 'cap=&From:']),
-      c1.join(['type=spinedit', 'pos=30,146,110,0', 'props=-1,5000,1', 'val='+str(op_offset1)]),
-      c1.join(['type=label', 'pos=120,128,230,0', 'cap=&To:']),
-      c1.join(['type=spinedit', 'pos=120,146,200,0', 'props=-1,5000,1', 'val='+str(op_offset2)]),
-      c1.join(['type=button', 'pos=60,190,160,0', 'cap=OK', 'props=1']),
-      c1.join(['type=button', 'pos=164,190,264,0', 'cap=Cancel']),
+      c1.join(['type=check', 'pos=6,102,300,0', 'cap=Numeric (treat beginning as number)', 'val='+op_numeric]),
+      c1.join(['type=label', 'pos=6,130,300,0', 'cap=Sort only by substring, offsets 0-based:']),
+      c1.join(['type=label', 'pos=30,152,130,0', 'cap=&From:']),
+      c1.join(['type=spinedit', 'pos=30,170,110,0', 'props=-1,5000,1', 'val='+str(op_offset1)]),
+      c1.join(['type=label', 'pos=120,152,230,0', 'cap=&To:']),
+      c1.join(['type=spinedit', 'pos=120,170,200,0', 'props=-1,5000,1', 'val='+str(op_offset2)]),
+      c1.join(['type=button', 'pos=60,210,160,0', 'cap=OK', 'props=1']),
+      c1.join(['type=button', 'pos=164,210,264,0', 'cap=Cancel']),
       ])
     
-    res = dlg_custom('Sort lines', size_x, size_y, text)
+    res = dlg_custom('Sort', size_x, size_y, text)
     if res is None: return
     btn, text = res
     if btn != id_ok: return
@@ -124,11 +148,13 @@ def do_dialog():
     ini_write(fn_ini, section, 'nocase', text[id_nocase])
     ini_write(fn_ini, section, 'del_dup', text[id_del_dup])
     ini_write(fn_ini, section, 'del_sp', text[id_del_sp])
+    ini_write(fn_ini, section, 'numeric', text[id_numeric])
     
     is_rev = text[id_rev]=='1'
     is_nocase = text[id_nocase]=='1'
     is_del_dup = text[id_del_dup]=='1'
     is_del_sp = text[id_del_sp]=='1'
+    is_numeric = text[id_numeric]=='1'
     offset1 = int(text[id_offset1])
     offset2 = int(text[id_offset2])
     
@@ -136,7 +162,7 @@ def do_dialog():
         msg_box('Incorrect offsets: %d..%d' % (offset1, offset2), MB_OK + MB_ICONERROR)
         return
     
-    return (is_rev, is_nocase, is_del_dup, is_del_sp, offset1, offset2)
+    return (is_rev, is_nocase, is_del_dup, is_del_sp, is_numeric, offset1, offset2)
     
 
 class Command:
